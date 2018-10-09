d6427173a18d93cf167e4b61541d6cc49407ccdd
Anmol Desai <anmoldesai4@gmail.com>
2018-10-09 16:44:49 +1100

    allow changing off passwords

--- src/components/PasswordChange.js
+++ src/components/PasswordChange.js
@@ -1,8 +1,67 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const PasswordChangePage = () =>
-  <div>
-    <h1>PasswordChangePage</h1>
-  </div>
+import { auth } from '../firebase';
 
-export default PasswordChangePage;
+const INITIAL_STATE = {
+  passwordOne: '',
+  passwordTwo: '',
+  error: null,
+};
+
+class PasswordChangeForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { ...INITIAL_STATE };
+  }
+
+  onSubmit = (event) => {
+    const { passwordOne } = this.state;
+
+    auth.doPasswordUpdate(passwordOne)
+      .then(() => {
+        this.setState({ ...INITIAL_STATE });
+      })
+      .catch(error => {
+        this.setState({'error': error });
+      });
+
+    event.preventDefault();
+  }
+
+  render() {
+    const {
+      passwordOne,
+      passwordTwo,
+      error,
+    } = this.state;
+
+    const isInvalid =
+      passwordOne !== passwordTwo ||
+      passwordOne === '';
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          value={passwordOne}
+          onChange={event => this.setState({ 'passwordOne' :event.target.value})}
+          type="password"
+          placeholder="New Password"
+        />
+        <input
+          value={passwordTwo}
+          onChange={event => this.setState({ 'passwordTwo': event.target.value})}
+          type="password"
+          placeholder="Confirm New Password"
+        />
+        <button disabled={isInvalid} type="submit">
+          Reset My Password
+        </button>
+
+        { error && <p>{error.message}</p> }
+      </form>
+    );
+  }
+}
+
+export default PasswordChangeForm;
